f8c7130abc238373e98fe1dc2ab937e745ef6f21
Alon Zelinsky <alon4688@gmail.com>
2023-06-04 14:24:28 +0300

    fix: delete publish

--- enums/publish.ts
+++
@@ -1,6 +0,0 @@
-export enum Publish {
-    BASE_TACTICS = "base_military_tactics",
-    DOMAIN_KNOWLEDGE = "domain_knowledge",
-    FIGHT_STRATEGY = "fight_strategy",
-    INSTRUCTIONS = "professional_orders",
-}
