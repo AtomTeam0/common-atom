251d5d4c47009d9d95b6b03458f13854c9a95104
Edan Ofer <edan.ofer@gmail.com>
2023-06-04 11:29:44 +0300

    feat: ISubjectQuery added

--- interfaces/subject.interface.ts
+++ interfaces/subject.interface.ts
@@ -1,26 +1,35 @@
-import { Compound } from "../enums/Compound";
-import { Degree } from "../enums/Degree";
-import { Job } from "../enums/Job";
-import { Publish } from "../enums/publish";
-import {CombatShapes} from "../enums/CombatShapes";
-import { Functions } from "../enums/Functions";
-import { IBook } from "./book.interface";
+import { Compound } from '../enums/Compound';
+import { Degree } from '../enums/Degree';
+import { Job } from '../enums/Job';
+import { Publish } from '../enums/publish';
+import { CombatShapes } from '../enums/CombatShapes';
+import { Functions } from '../enums/Functions';
+import { IBook } from './book.interface';
+import { IPaginationQuery } from './helpers/paginator.interface';
 
 export interface IPageRange {
-    from: number;
-    to: number;
-  }
+  from: number;
+  to: number;
+}
 
 export interface ISubject {
-    _id?: string;
-    title: string;
-    priority: number;
-    pages: (IPageRange | number)[];
-    book: string | IBook;
-    jobs: Job[];
-    publishType: Publish[];
-    degrees: Degree[];
-    compounds: Compound[];
-    combatShapes: CombatShapes[];
-    functions: Functions[];
-  }
+  _id?: string;
+  title: string;
+  priority: number;
+  pages: (IPageRange | number)[];
+  book: string | IBook;
+  jobs: Job[];
+  publishType: Publish[];
+  degrees: Degree[];
+  compounds: Compound[];
+  combatShapes: CombatShapes[];
+  functions: Functions[];
+}
+
+export interface ISubjectQuery extends IPaginationQuery {
+  bookId?: string;
+  jobs?: Job[];
+  publishType?: Publish[];
+  degrees?: Degree[];
+  compounds?: Compound[];
+}
