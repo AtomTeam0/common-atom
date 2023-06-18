b45c90b5ea053a0fbd9d675aa6677ea3beb65435
Alon Zelinsky <alon4688@gmail.com>
2023-06-18 14:41:44 +0300

    feat: plugins names

---
+++ enums/Plugins.ts
@@ -0,0 +1,7 @@
+export enum Plugins {
+    AGGREGATE = "aggregate_plugin",
+    BLOB = "blob_plugin",
+    INDEX = "index_plugin",
+    POPULATE = "populate_plugin",
+    SOCKET = "socket_plugin",
+  }
