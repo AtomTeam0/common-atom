68a99434aa9dd51b6ccad377501708458eb73866
Alon Zelinsky <alon4688@gmail.com>
2023-06-18 14:45:30 +0300

    fix: rpcPayload can get array of plugins to skip

--- interfaces/helpers/rpcPayload.interface.ts
+++ interfaces/helpers/rpcPayload.interface.ts
@@ -1,7 +1,8 @@
+import { Plugins } from "../../enums/Plugins";
 import { IUser } from "../user.interface";
 
 export interface IRPCPayload {
   user?: IUser;
   params?: { [k: string]: any };
-  skipPlugins: boolean;
+  skipPlugins: Plugins[];
 }
