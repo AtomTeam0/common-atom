9a71e8e842403b991381464376a61e011a0652a3
NoaGrundland <noagrun.office@gmail.com>
2023-08-29 16:38:05 +0300

    fix: set corp type to string | IUnit

--- interfaces/book.interface.ts
+++ interfaces/book.interface.ts
@@ -1,13 +1,14 @@
+import { IUnit } from "./unit.interface";
+
 export interface IBook {
-    _id?: string;
-    title: string;
-    priority: number;
-    length: number;
-    isActive: boolean;
-    updatedAt: Date;
-    createdAt: Date;
-    pdf: string;
-    thumbNail: string;
-    corp: string
-  }
-  
+  _id?: string;
+  title: string;
+  priority: number;
+  length: number;
+  isActive: boolean;
+  updatedAt: Date;
+  createdAt: Date;
+  pdf: string;
+  thumbNail: string;
+  corp: string | IUnit;
+}
