27ab2763db0e86a54e70421e3d0d2ec3d8f87eae
Eytan Ivan Reuven <209435924@idf.il>
2023-11-30 12:31:16 +0200

    :art: fix type

--- interfaces/item.interface.ts
+++ interfaces/item.interface.ts
@@ -71,4 +71,4 @@ export interface IFilteredItem extends IPagingQuery {
   optionalFilters?: string[];
 }
 
-export type GroupedItems = {[key: number]: IFilter[]}
+export type GroupedItems = {[key: number]: IItem[]}
