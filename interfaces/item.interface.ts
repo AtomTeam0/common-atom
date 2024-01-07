edfe3fdc31c4a730c6928db273087ddb8b96e503
Asaf Jacobson <212400964@idf.il>
2024-01-07 17:36:52 +0200

    :recycle: small fixes

--- interfaces/item.interface.ts
+++ interfaces/item.interface.ts
@@ -1,12 +1,13 @@
-import {Category} from "../enums/Category";
-import {ContentType} from "../enums/ContentType";
-import {Corp} from "../enums/Corp";
-import {Grade} from "../enums/Grade";
-import {Section} from "../enums/Section";
-import {IUnit} from "./unit.interface";
-import {IUser, IUserItemPatch} from "./user.interface";
-import {IPaginationQuery, IPagingQuery} from "./helpers/paginator.interface";
-import {IFilter} from "./filter.interface";
+import { Category } from "../enums/Category";
+import { ContentType } from "../enums/ContentType";
+import { Grade } from "../enums/Grade";
+import { Section } from "../enums/Section";
+import { IUnit } from "./unit.interface";
+import { IUser, IUserItemPatch } from "./user.interface";
+import { IPaginationQuery, IPagingQuery } from "./helpers/paginator.interface";
+import { IFilter } from "./filter.interface";
+import { Status } from "../enums/Status";
+import { IWorld } from "../dist/interfaces/world.interface";
 
 export interface IItemQuery extends IPaginationQuery {
   filters?: string[];
@@ -49,21 +50,17 @@ export interface IItem extends IUserItemPatch {
   title: string;
   description: string;
   views: number;
-  timeToRead: number;
   priority: number;
+  timeToRead: number;
   isActive: boolean;
-  isByMission: boolean;
-  sections: Section[];
-  categories: Category[] | Category;
-  corps: Corp[];
-  grade: Grade;
   contentType: ContentType;
+  status: Status;
   thumbNail: string;
+  unit: string | IUnit;
+  filters: string[] | IFilter[];
+  world: string | IWorld;
   contentId: string;
   editedBy: string | IUser;
-  filters: string[] | IFilter[];
-  unit: string | IUnit;
-  similarItems: string[] | IItem[];
 }
 
 export interface IFilteredItem extends IPagingQuery {
@@ -71,4 +68,4 @@ export interface IFilteredItem extends IPagingQuery {
   groupedByFilters?: string[];
 }
 
-export type GroupedItems = {[key: string]: IItem[]}
+export type GroupedItems = { [key: string]: IItem[] };
