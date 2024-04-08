395249c684667b0950a7df98b61aecdd1ef343df
Eilam Hasson <eilamhasson1@gmail.com>
2024-04-08 16:11:14 +0300

    🏷️ add helper type for express request

---
+++ interfaces/helpers/request.type.ts
@@ -0,0 +1,21 @@
+import { Request as expressRequest } from "express";
+
+type RequestParams = Record<string, string>;
+type RequestBody = object;
+type RequestQuery = Record<string, string | string[]>;
+
+type defaultTo<T, R> = T extends R ? T : R;
+
+export type Request<
+  T extends {
+    body?: RequestBody;
+    query?: RequestQuery;
+    params?: RequestParams;
+  } & Record<string, unknown>
+> = expressRequest<
+  defaultTo<T["params"], RequestParams>,
+  object,
+  defaultTo<T["body"], RequestBody>,
+  defaultTo<T["query"], RequestQuery>
+> &
+  Omit<T, "body" | "query" | "params">;
